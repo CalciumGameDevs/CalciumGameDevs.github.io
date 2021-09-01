55270397ff3edd9f52f9a5bdea29d279d48e4c84
3EGaming <coinman_1@icloud.com>
2021-09-01 18:13:40 +0100

    Update scripts

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,16 +1,3 @@
-var head = document.getElementsByTagName('HEAD')[0];
-
-function LoadCss(cssPath) {
-    var thisCss = document.createElement('link');
-    thisCss.rel = 'stylesheet';
-    thisCss.type = 'text/css';
-    thisCss.href = cssPath; 
-
-    head.appendChild(thisCss); 
-}
-
-LoadCss("/assets/css/navbar.css");
-
 var navBar = document.getElementById("nb");
 
 navBar.innerHTML = '<ul><li><a href="/">Home</a></li><li><a href="/games/">Our Games</a></li><li><a href="/about/">About Us</a></li></ul>';
