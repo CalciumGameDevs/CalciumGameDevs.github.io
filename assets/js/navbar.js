f3d7479f9bc7207926b7373637fa54b86c535d21
3EGaming <coinman_1@icloud.com>
2021-09-01 18:07:42 +0100

    Add navigation bar

---
+++ assets/js/navbar.js
@@ -0,0 +1,16 @@
+var head = document.getElementsByTagName('HEAD')[0];
+
+function LoadCss(cssPath) {
+    var thisCss = document.createElement('link');
+    thisCss.rel = 'stylesheet';
+    thisCss.type = 'text/css';
+    thisCss.href = cssPath; 
+
+    head.appendChild(thisCss); 
+}
+
+LoadCss("/assets/js/navbar.css");
+
+var navBar = document.getElementById("nb");
+
+navBar.innerHTML = '<ul><li><a href="/">Home</a></li><li><a href="/games/">Our Games</a></li><li><a href="/about/">About Us</a></li></ul>';
