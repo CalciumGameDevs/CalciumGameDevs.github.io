6f9982d502f7902f1d125c146afb800a43882d7e
3EGaming <3EGaming@users.noreply.github.com>
2021-09-05 19:37:52 +0100

    Better loading from buttons

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,6 +1,4 @@
 var navBar = document.getElementById("nb");
 if (urlVariables["nonavbar"] == undefined) {
     navBar.innerHTML = '<ul><li><a href="/#">Home</a></li><li><a href="/#games">Our Games</a></li><li><a href="/#about">About Us</a></li></ul><br>';
-} else {
-    navBar.innerHTML = '<br>';
 }
